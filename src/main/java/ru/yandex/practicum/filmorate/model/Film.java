63369a8edb1cd132d9a20bdd1abb3461ffe8e96f
IEMeniailo <menyailo96@mail.ru>
2025-08-25 23:20:54 +0300

    Выполнено ТЗ-10 без доп задания.

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.*;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -8,17 +7,8 @@ import java.time.LocalDate;
 @Data
 public class Film {
     private Long id;
-
-    @NotBlank(message = "Название не может быть пустым")
     private String name;
-
-    @Size(max = 200, message = "Максимальная длина символов: 200 символов")
     private String description;
-
-    @NotNull(message = "Должна быть указана дата релиза фильма")
-    @PastOrPresent(message = "Дата релиза не может быть в будущем")
     private LocalDate releaseDate;
-
-    @Positive(message = "Продолжительность фильма должна быть положительным числом")
     private int duration;
 }

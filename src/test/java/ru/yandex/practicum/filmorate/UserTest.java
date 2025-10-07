da0bba696030ef2dc9bcf3b08a9f08f61f1efcaa
IEMeniailo <menyailo96@mail.ru>
2025-10-07 23:03:29 +0300

    Выполнено задание ТЗ-12 (1)

--- src/test/java/ru/yandex/practicum/filmorate/UserTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserTest.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate;
 
 import jakarta.validation.ConstraintViolation;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
